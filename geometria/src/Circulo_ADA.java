270dfec39feb35f3bc8aaff631af48090302a952
Anais <anaisdeltellanton@gmail.com>
2022-02-21 19:56:20 +0100

    versión refactorizada

--- geometria/src/Circulo_ADA.java
+++ geometria/src/Circulo_ADA.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_ADA extends FiguraGeometrica_ADA {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_ADA(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
